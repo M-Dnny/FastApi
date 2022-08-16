9b5b8d98f90457a6dabada450ce7311a1a0c9fb0
M-Dnny <mansuridanish14@gmail.com>
2022-08-16 23:21:25 +0530

    Latest changes

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origin=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origin=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 app.include_router(users.routers)
 app.include_router(posts.routers)
